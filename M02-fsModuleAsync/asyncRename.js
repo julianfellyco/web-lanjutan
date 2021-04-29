caa3ba435d0b09f15bfb8063cfb262d8e461e310
julianfellyco <julianfellyco85@gmail.com>
2021-04-29 12:35:41 +0700

    web-lanjutan

---
+++ M02-fsModuleAsync/asyncRename.js
@@ -0,0 +1,11 @@
+  
+const fs = require("fs");
+
+fs.rename("mwpagi.json", "mwsore.json", (err) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  //done
+  console.log("Berhasil mengganti nama!");
+});
